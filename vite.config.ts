8709b50ab1ed64710a374bfba8efb8741c4228c7
Tyler Beck <t.beck1990@gmail.com>
2022-07-23 16:21:27 -0400

    copy package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'package.json',
+          dest: '',
+        },
+      ],
+    }),
+  ],
   resolve: {
     alias: {
 
